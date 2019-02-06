57dd06373bbc1d5ec607d676f99d29750d1eb98d
dfspray <daniel_spray@comcast.net>
2019-02-06 11:29:18 -0800

    IC-2018.2.5 <allth@DESKTOP-CUFGGFU Create git.xml, filetypes.xml, vcs.xml, colors.scheme.xml, laf.xml, Class.java, github_settings.xml, jdk.table.xml, Default.xml, debugger.xml, path.macros.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I did it too!");
+        System.out.println("Did I make a branch?");
+        System.out.println("Yes you did!");
+    }
+}
